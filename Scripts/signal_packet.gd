8d8b99b4bb7b34bcdaec4f70b5bc1a5db6a05e81
Yann in 't Veld <yannintveld@gmail.com>
2026-04-18 20:46:17 +0200

    Added instruction package sprites

--- Scripts/signal_packet.gd
+++ Scripts/signal_packet.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
 @export
-var instruction: String
+var instruction: String = ""
 @export
 var direction: Vector2
 const SPEED = 400.
 
+@export var sprites: Dictionary[String, CompressedTexture2D] = {"": null, "down": null, "up": null, "left": null, "right": null}
+
+@onready var _my_sprite_renderer: Sprite2D = get_node("./Sprite2D")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_my_sprite_renderer.texture = sprites[instruction]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
