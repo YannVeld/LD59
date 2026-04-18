398148ea5011726fab7183f4cf2aafef7e8451bb
GR <gautamr.qv@gmail.com>
2026-04-18 17:15:07 +0200

    Add aim guide

---
+++ Scripts/aim_guide.gd
@@ -0,0 +1,21 @@
+extends Line2D
+
+const LENGTH=1000
+var end_point
+@onready
+var transmitter = $"../Transmitter"
+var start_point
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start_point = transmitter.position
+	add_point(start_point)
+	end_point = start_point+(get_viewport().get_mouse_position()-start_point).normalized()*LENGTH
+	add_point(end_point)
+	return	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	end_point = start_point+(get_viewport().get_mouse_position()-start_point).normalized()*LENGTH
+	set_point_position(1, end_point)
+	pass
