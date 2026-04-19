bbbf353993144fc838379af0c0cee2a7c0a6ce4a
GR <gautamr.qv@gmail.com>
2026-04-19 22:40:05 +0200

    make a level 4, beware the oil slicks

--- Scripts/game_menu.gd
+++ Scripts/game_menu.gd
@@ -5,11 +5,9 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://level1.tscn")
@@ -19,3 +17,12 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://level3.tscn")
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://level4.tscn")
+
+func _on_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://level5.tscn")
+
+func _on_button_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://level6.tscn")
