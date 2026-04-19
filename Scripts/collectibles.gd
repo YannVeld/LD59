51ae234d7a2b7ef6c08e06a0c67afb19ef9da4cc
GR <gautamr.qv@gmail.com>
2026-04-19 17:44:24 +0200

    Adjust zorder fixon trees, remove zorder fix on vines

--- Scripts/collectibles.gd
+++ Scripts/collectibles.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var robot: Area2D = $"../Robot"
+@export var prevent_zorder_changes = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,11 +10,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if robot.position.y < position.y:
-		$Sprite2D.z_index = 2
-	else:
-		$Sprite2D.z_index = 0
-	pass
+	if not prevent_zorder_changes:
+		if robot.position.y < position.y:
+			$Sprite2D.z_index = 2
+		else:
+			$Sprite2D.z_index = 0
+		pass
 	
 func turn_off_shader():
 	$Sprite2D.material.set_shader_parameter('do_blinking', false)
