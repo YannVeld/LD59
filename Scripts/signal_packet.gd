3c0fc48d5a3a3036a5f2ef4c00a254ec7d467b7c
GR <gautamr.qv@gmail.com>
2026-04-19 12:42:03 +0200

    git status

--- Scripts/signal_packet.gd
+++ Scripts/signal_packet.gd
@@ -20,4 +20,8 @@ func _process(delta: float) -> void:
 	return
 
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
+	print('body entered')
+	if body.is_in_group("Signal Absorbers"):
+		queue_free()
+	if body.is_in_group("Mirrors"):
+		print('Hit Mirror!')
