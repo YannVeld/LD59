4c01afc5a6568e3fbc3a7d72b194251517fd6cda
Yann in 't Veld <yannintveld@gmail.com>
2026-04-20 23:43:12 +0200

    Fixed scene fader bug

--- Scripts/fader.gd
+++ Scripts/fader.gd
@@ -1,12 +1,16 @@
 extends ColorRect
 
-@export var FADE_IN_SPEED = 0.5
-@export var FADE_OUT_SPEED = 0.7
+@export var FADE_IN_SPEED = 1.0
+@export var FADE_OUT_SPEED = 1.0
 var fading_in = true
 var fading_out = false
 
+signal on_fade_in_finished
+signal on_fade_out_finished
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	visible = true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,9 +20,15 @@ func _process(delta: float) -> void:
 		if color[3]<0:
 			color[3]=0
 			fading_in=false
+			on_fade_in_finished.emit()
 		
 	if fading_out:
 		color[3] += FADE_OUT_SPEED*delta
+		
+		if color[3] >= 1.0:
+			color[3]=1.0
+			fading_out = false
+			on_fade_out_finished.emit()
 		
 func _on_robot_takeoff_flying_starts() -> void:
 	fading_out=true
