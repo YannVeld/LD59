3fc32783d49fa06b5205167ce8c2b7466515f7dc
Yann in 't Veld <yannintveld@gmail.com>
2026-04-20 23:29:05 +0200

    Aliens now jump (I already had the animation)

--- Scripts/alien.gd
+++ Scripts/alien.gd
@@ -7,11 +7,19 @@ const SPEED: float = 8.0
 
 const DISTANCE_DELTA: float = 2.0
 
+const JUMP_CHANCE: float = 0.25
+const MIN_JUMPS_COUNT: int = 2
+const MAX_JUMPS_COUNT: int = 4
+
+
 var _target_position: Vector2
 var _orig_position: Vector2
 var _wander_timer: float
 var _wander_time: float
 var velocity: Vector2 = Vector2.ZERO
+
+var _is_jumping: bool = false
+var _jumps_to_go: int = 3
 
 @onready var _my_animator: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -29,14 +37,18 @@ func _ready() -> void:
 	_wander_time = randf_range(MIN_TIME_BETWEEN_WANDERING, MAX_TIME_BETWEEN_WANDERING)
 	_wander_timer = randf_range(0, MIN_TIME_BETWEEN_WANDERING)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	_wander_timer += delta
+func _make_new_decision() -> void:
+	if (randf() <= JUMP_CHANCE) and not _is_jumping:
+		_is_jumping = true
+		_jumps_to_go = randi_range(MIN_JUMPS_COUNT, MAX_JUMPS_COUNT)
+		_my_animator.play("jump")
 	
-	if _wander_timer >= _wander_time:
-		_wander_timer = 0.0
-		_wander_time = randf_range(MIN_TIME_BETWEEN_WANDERING, MAX_TIME_BETWEEN_WANDERING)
-		_target_position = _get_random_wander_position()
+	_wander_timer = 0.0
+	_wander_time = randf_range(MIN_TIME_BETWEEN_WANDERING, MAX_TIME_BETWEEN_WANDERING)
+	_target_position = _get_random_wander_position()
+
+func _do_wander(delta: float) -> void:
+	_wander_timer += delta
 	
 	var _vec_to_target = _target_position - position
 	var _dist_to_target = _vec_to_target.length()
@@ -51,3 +63,19 @@ func _process(delta: float) -> void:
 		_my_animator.play("idle")
 	else:
 		_my_animator.play("walk")
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if _wander_timer >= _wander_time:
+		_make_new_decision()
+	
+	if not _is_jumping:
+		_do_wander(delta)
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	if _my_animator.animation != "jump": return
+	
+	_jumps_to_go -= 1
+	if _jumps_to_go <= 0:
+		_is_jumping = false
